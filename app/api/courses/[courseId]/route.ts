e9e0d53a4ab9f873c5351c9e59a54e922d19c092
padi-dev-manhtd <manhtd@paditech.com>
2024-08-12 15:05:18 +0700

    edit course title description

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const { courseId } = params;
+    const values = await req.json();
+    const course = await db.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: { ...values },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("🚀 ~ PATCH ~ error:", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
