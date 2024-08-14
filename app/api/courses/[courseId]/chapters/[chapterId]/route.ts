d772ddf49f9f5075f1a97937ee5848f6a224caff
padi-dev-manhtd <manhtd@paditech.com>
2024-08-14 21:11:27 +0700

    update title chapter

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const { courseId, chapterId } = params;
+    const { isPublished, ...values } = await req.json();
+    const ownerCourse = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+
+    if (!ownerCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: {
+        id: chapterId,
+        courseId: courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("🚀 ~ error:", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
